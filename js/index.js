f481f93412e8e51d8b1187210c407340f7019c46
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-01-15 23:39:13 +0200

    did a js part of burger menu

--- js/index.js
+++ js/index.js
@@ -2,4 +2,6 @@
 function showNav() {
     let toggle = document.getElementById('toggle');
     toggle.classList.toggle('toggle--active');
+    let nav = document.getElementById('nav');
+    nav.classList.toggle('active');
 }
